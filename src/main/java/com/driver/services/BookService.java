a9742064d9ee004fb3327b99564eb698a8fb7a83
Yogesh <bc21143@smsvaranasi.in>
2023-01-24 00:01:09 +0530

    initial

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -1,6 +1,8 @@
 package com.driver.services;
 
+import com.driver.models.Author;
 import com.driver.models.Book;
+import com.driver.repositories.AuthorRepository;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +16,46 @@ public class BookService {
     @Autowired
     BookRepository bookRepository2;
 
+
+    @Autowired
+    AuthorRepository authorRepository1;
+
     public void createBook(Book book){
+
+
+        //Save the author in book
+
+        int authorId = book.getAuthor().getId();
+
+        Author author =  authorRepository1.findById(authorId).get();
+
+        //Update the bookList written by Author
+        author.getBooksWritten().add(book);
+
+        //Updated the book
+        book.setAuthor(author);
+        //bookRepository2.save(book);
         bookRepository2.save(book);
+
+        authorRepository1.save(author);
+
+
+
     }
 
+    //This has to be rectified....and given a thought
+
     public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> books = bookRepository2.findBooksByGenreAuthor(genre, author, available); //find the elements of the list by yourself
-        return books;
+
+
+        if(genre != null && author != null){
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        }else if(genre != null){
+            return bookRepository2.findBooksByGenre(genre, available);
+        }else if(author != null){
+            return bookRepository2.findBooksByAuthor(author, available);
+        }else{
+            return bookRepository2.findByAvailability(available);
+        }
     }
 }
